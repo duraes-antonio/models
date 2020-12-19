b10651f9bb8e7d667b907a62aa64206eaa195750
duraes-antonio <garotoseis@gmail.com>
2020-12-18 22:55:15 -0300

    FIX: Remoção de pasta com checkpoint inicial

--- research/train-pqr.sh
+++ research/train-pqr.sh
@@ -6,7 +6,6 @@ DATASET_DIR="datasets"
 # Set up the working directories.
 PQR_FOLDER="PQR"
 EXP_FOLDER="exp/train_on_trainval_set"
-INIT_FOLDER="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/${EXP_FOLDER}/init_models"
 TRAIN_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/${EXP_FOLDER}/train"
 DATASET="${WORK_DIR}/${DATASET_DIR}/${PQR_FOLDER}/tfrecord"
 
@@ -27,6 +26,5 @@ python3 "${WORK_DIR}"/train.py \
   --train_batch_size=4 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
   --fine_tune_batch_norm=true \
-  --tf_initial_checkpoint="${INIT_FOLDER}/deeplabv3_pascal_train_aug/model.ckpt" \
   --train_logdir="${TRAIN_LOGDIR}" \
   --dataset_dir="${DATASET}"
